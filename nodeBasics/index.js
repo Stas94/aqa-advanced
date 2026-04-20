e2d03dc0136eea6d26a1f88822778090f2f69335
Stanislav Puha <stasyan083@gmail.com>
2026-04-20 12:07:01 +0200

    Push chahge

--- nodeBasics/index.js
+++
@@ -1,5 +0,0 @@
-import chalk from 'chalk';
-
-console.log(chalk.blue('Hello') + ' ' + chalk.yellow('World') + chalk.red('!'));
-
-console.log(chalk.black('This') + ' ' + chalk.green('my') + ' ' + chalk.red('first') + ' ' + chalk.yellow('text'));
