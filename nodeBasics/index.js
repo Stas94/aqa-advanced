7309fc62195ad04f9d914ef612f779d10a0ed28b
Stanislav Puha <stasyan083@gmail.com>
2026-04-20 12:02:40 +0200

    Delete old file and Install dependencies chalk

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello') + ' ' + chalk.yellow('World') + chalk.red('!'));
+
+console.log(chalk.black('This') + ' ' + chalk.green('my') + ' ' + chalk.red('first') + ' ' + chalk.yellow('text'));
