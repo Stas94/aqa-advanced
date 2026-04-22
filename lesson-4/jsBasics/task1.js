93814eefaf7e61aa0eef3e0576e77b4fec534028
Stanislav Puha <stasyan083@gmail.com>
2026-04-22 12:14:52 +0200

    JavaScript primitive data types

---
+++ lesson-4/jsBasics/task1.js
@@ -0,0 +1,11 @@
+let number = 5;
+let string = 'Hello world!';
+let boolean = true;
+let empty = null;
+let notDefined;
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(empty);
+console.log(notDefined);
---
+++ lesson-4/jsBasics/task4.1.js
@@ -0,0 +1,5 @@
+let radius = 30;
+let result = Math.PI * Math.pow(radius, 2);
+
+console.log(result);
+
