1425f9cc3583de1ba6ca2acfdf666b8162c8ea32
Stanislav Puha <stasyan083@gmail.com>
2026-04-28 14:16:49 +0200

    JavaScript conditions and loops (#4)

---
+++ lesson-5/task1.js
@@ -0,0 +1,13 @@
+let averageGrade = 63;
+
+if (averageGrade < 60) {
+  console.log("Успішність студента: Незадовільно");
+} else if (averageGrade >= 60 && averageGrade <= 70) {
+  console.log("Успішність студента: Задовільно");
+} else if (averageGrade >= 71 && averageGrade <= 80) {
+  console.log("Успішність студента: Добре");
+}else if (averageGrade >= 81 && averageGrade <= 90) {
+  console.log("Успішність студента: Дуже добре");
+}else if (averageGrade >= 91 && averageGrade <= 100) {
+  console.log("Успішність студента: Відмінно");
+}
---
+++ lesson-5/task4.js
@@ -0,0 +1,10 @@
+const number = 5;
+let output = "";
+let i = 1;
+
+while (i <= 10) {
+  output = `${number} * ${i} = ${number * i}`;
+  console.log(output);
+  
+  i++;
+}
