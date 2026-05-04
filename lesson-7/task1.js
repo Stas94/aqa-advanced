849232773195f590d44bd89bfabea9c9ce67d280
Stanislav Puha <stasyan083@gmail.com>
2026-05-04 22:42:22 +0200

    Advanced function usage

---
+++ lesson-7/task1.js
@@ -0,0 +1,21 @@
+
+function handleNum(num, onEven, onOdd) {
+  if (num % 2 === 0) {
+    onEven();
+  } else {
+    onOdd();
+  }
+}
+
+
+function handleEven() {
+  console.log("number is even");
+}
+
+function handleOdd() {
+  console.log("number is odd");
+}
+
+
+handleNum(4, handleEven, handleOdd);
+handleNum(7, handleEven, handleOdd);
