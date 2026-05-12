2d155de6249823d9ace4a9074c52175c27de5079
Stanislav Puha <stasyan083@gmail.com>
2026-05-12 08:42:56 +0200

    Arrays in JavaScript update version

--- lesson-8/task2.js
+++ lesson-8/task2.js
@@ -1,8 +1,19 @@
 
+// Full version
+
 const initialArray = [1, 2, 3, 4, 5];
 
 const mutatedArray = initialArray.map((value, index) => {
     return value * index;
 });
+
+console.log(mutatedArray);
+
+
+// abridged version
+
+const initialArray = [1, 2, 3, 4, 5];
+
+const mutatedArray = initialArray.map((value, index) => value * index);
 
 console.log(mutatedArray);
