7e67bad4eba6f1eea9d66f74b53d29ef2d6e9f9f
Stanislav Puha <stasyan083@gmail.com>
2026-05-14 17:24:32 +0200

    Objects in JavaScript (#8)
    * Objects in JavaScript
    
    * Objects in JavaScript update version

---
+++ lesson-9/task1.js
@@ -0,0 +1,11 @@
+
+const student = {};
+
+student.name = {
+  firstName: "John",
+  lastName: "Smith",
+}
+student.age = 25;
+student.courses = ["Математика", "Історія", "Програмування"];
+
+console.log(student);
---
+++ lesson-9/task2.js
@@ -0,0 +1,11 @@
+
+const book = {
+    title: "Кобзар",
+    author: "Тарас Шевченко",
+    year: 1840
+};
+
+const { title, author } = book;
+
+console.log("Назва книги:", title);
+console.log("Автор книги:", author);
