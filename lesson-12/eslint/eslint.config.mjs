a456e8f2ef894e63698489a3ab4ead57698bd0a2
Stanislav Puha <stasyan083@gmail.com>
2026-06-01 11:06:28 +0200

    Code quality (#11)

---
+++ lesson-12/eslint/eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: {
+            js,
+            '@stylistic': stylistic,
+        },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.node },
+        rules: {
+            'no-useless-escape': 'error',
+            'no-shadow': 'error',
+            '@stylistic/block-spacing': ['error', 'always'],
+            '@stylistic/arrow-spacing': 'error',
+            '@stylistic/comma-dangle': ['error', 'always-multiline'],
+            '@stylistic/arrow-parens': ['error', 'always'],
+            '@stylistic/curly-newline': ['error', 'always'],
+            '@stylistic/indent': ['error', 4],
+        },
+    },
+]);
